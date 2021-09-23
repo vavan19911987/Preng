d48db54200042582c6b0ac711aff66eb0c496536
Vavan19911987 <vavan7520@gmail.com>
2021-09-23 23:26:01 +0300

    сделал серцию 3

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,36 @@
 window.addEventListener('DOMContentLoaded', function () {
     "user strict";
+    const slider = tns({
+        container: '.about__slider',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        nav: false,
+        controls: false,
+        speed: 1300,
+        
+        responsive: {
+            320: {
+                nav: true
+            },
+            992: {
+                nav: true,
+            },
+        }
 
+    });
+
+    document.querySelector('.prev').addEventListener('click', function () {
+        slider.goTo('prev');
+    });
+    document.querySelector('.next').addEventListener('click', function () {
+        slider.goTo('next');
+    });
    
     new WOW().init();
 
 });
+
+
 
 
